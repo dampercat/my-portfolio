149683d57ac4d67694cf19359790d49eafcda4f1
dampercat <mylesthestudent@gmail.com>
2025-06-20 03:56:35 +0700

    Fixing card displays, fixing slider displays, slider response, play-pause response

--- script.js
+++ script.js
@@ -25,6 +25,71 @@ themeSwitch.addEventListener('click', () => {
         }
 })
 
+document.querySelectorAll(".music-card").forEach(card => {
+    const song = card.querySelector(".song");
+    const progress = card.querySelector(".progress");
+    const ctrlIcon = card.querySelector(".ctrlIcon");
+    let progressUpdater;
+
+    song.pause();
+    song.currentTime = 0;
+
+    song.onloadedmetadata = () => {
+        progress.max = song.duration;
+        progress.value = song.currentTime;
+    };
+
+    ctrlIcon.parentElement.addEventListener('click', () => {
+        if (song.paused) {
+
+            document.querySelectorAll(".song").forEach(otherSong => {
+            if (otherSong !== song) {
+                otherSong.pause();
+                otherSong.closest('.music-card')
+                    .querySelector('.ctrlIcon')
+                    .classList.remove("fa-pause");
+                otherSong.closest('.music-card')
+                    .querySelector('.ctrlIcon')
+                    .classList.add("fa-play");
+            }
+        });
+            song.play();
+            ctrlIcon.classList.add("fa-pause");
+            ctrlIcon.classList.remove("fa-play");
+
+            progressUpdater = setInterval(() => {
+                progress.value = song.currentTime;
+            }, 100);
+        } else {
+            song.pause();
+            ctrlIcon.classList.remove("fa-pause");
+            ctrlIcon.classList.add("fa-play");
+
+            clearInterval(progressUpdater);
+        }
+    });
+
+    progress.addEventListener('input', () => {
+        song.currentTime = progress.value;
+        if (song.paused) {
+            song.play();
+            ctrlIcon.classList.add("fa-pause");
+            ctrlIcon.classList.remove("fa-play");
+
+            progressUpdater = setInterval(() => {
+                progress.value = song.currentTime;
+            }, 100);
+        }
+    });
+
+    song.addEventListener("ended", () => {
+        ctrlIcon.classList.remove("fa-pause");
+        ctrlIcon.classList.add("fa-play");
+        progress.value = song.currentTime;
+    });
+});
+
+/* Original play pause functions
 const progress = document.getElementById("progress");
 const song = document.getElementById("song");
 const ctrlIcon = document.getElementById("ctrlIcon");
@@ -68,4 +133,4 @@ progress.onchange = function () {
             progress.value = song.currentTime;
         }, 500);
     }
-};
+};*/
